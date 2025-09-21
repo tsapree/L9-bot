eef92007f3e3bb9cccaf068a33afc8e00e27d38f
stakhov-pv <stakhov.pv@gmail.com>
2025-09-21 22:01:43 +0500

    empty spring project

---
+++ README.md
@@ -0,0 +1,4 @@
+# L9 Games Telegram Bot
+
+Local Run:
+ - mvn clear install spring-boot:run
---
+++ l9-bot-app/src/main/java/biz/atomeo/l9/L9BotApplication.java
@@ -0,0 +1,13 @@
+package biz.atomeo.l9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L9BotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(L9BotApplication.class, args);
+	}
+
+}
