9d3b88113a4084db9a88d93432c4196352c1c7d0
Pavel Stakhov <tsapree@gmail.com>
2025-09-29 00:55:43 +0500

    enable/disable/stats controller

--- l9-bot-app/src/main/java/biz/atomeo/l9/service/BotStateProvider.java
+++ l9-bot-app/src/main/java/biz/atomeo/l9/service/BotStateProvider.java
@@ -1,10 +1,52 @@
 package biz.atomeo.l9.service;
 
+import biz.atomeo.l9.openapi.model.L9StatRs;
+import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
+@Slf4j
 public class BotStateProvider {
+    private final InMemorySessionProvider inMemorySessionProvider;
+
+    private boolean enabled = true;
+
+    @Getter
+    private L9StatRs.BotStatusEnum status = L9StatRs.BotStatusEnum.STARTING;
+
+    @PostConstruct
+    private void started() {
+        log.info("Bot state provider started");
+        enableBot();
+    }
+
     public boolean isBotActive() {
-        return true; //TODO: on/off realization
+        return enabled;
+    }
+
+    public void disableBot() {
+        if (enabled) {
+            enabled = false;
+            status = L9StatRs.BotStatusEnum.SHUTTING_DOWN;
+            //TODO
+            status = L9StatRs.BotStatusEnum.DISABLED;
+        }
+    }
+
+    public void enableBot() {
+        if (!enabled) {
+            status = L9StatRs.BotStatusEnum.STARTING;
+            //TODO
+            status = L9StatRs.BotStatusEnum.ENABLED;
+            enabled = true;
+        }
+    }
+
+    public int getActiveUsers() {
+        return inMemorySessionProvider.getSessionsCount();
     }
 }
